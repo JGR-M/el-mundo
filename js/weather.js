c73c12d524307d4675c391d27e7a3553b5f46847
JGRM <german9989@outlook.com>
2019-03-09 22:29:30 -0600

    accessing api

---
+++ js/weather.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+    $('#submitWeather').click(function() {
+
+        var city = $('#city').val();
+
+        if(city != ''){
+
+            $.ajax({
+
+                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&APPID=4ed06e5c3a62464b150f64d2de562fea',
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data){
+                    var widget = show(data);
+
+                    $("#show").html(widget);
+
+                    $('#city').val('');
+                }
+
+
+            });
+
+
+        }else { 
+            $('#error').html("Field cannot be empty.");
+        }
+    })
+
+});
+
+
+function show(data){
+    return  "<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" + 
+            "<h3><strong>Temperature</strong>: "  + data.main.temp + "</h3>";
+}
+
+
+
+
+    // js for weather applicationCache, working
+
+    // connected to clasico.html,  button id submitWeather
